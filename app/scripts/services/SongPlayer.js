4caeacd9b0bf3c7cea070bdc4ce717fb9aff4c37
Sara Bilmes <saraesrig@gmail.com>
2017-06-19 12:34:17 +0300

    Checkpoint-angular10:complete but not working

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           var SongPlayer = {};
 
           /**
@@ -31,6 +31,12 @@
               formats: ['mp3'],
               preload: true
             });
+
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });
 
              SongPlayer.currentSong = song;
           };
@@ -67,6 +73,11 @@
  * @type {Object}
  */
           SongPlayer.currentSong = null;
+          /**
+ * @desc Current playback time (in seconds) of currently playing song
+ * @type {Number}
+ */
+         SongPlayer.currentTime = null;
 
           SongPlayer.play = function(song){
             song = song || SongPlayer.currentSong;
@@ -124,6 +135,17 @@
        }
            };
 
+           /**
+           * @function setCurrentTime
+           * @desc Set current time (in seconds) of currently playing song
+           * @param {Number} time
+           */
+           SongPlayer.setCurrentTime = function(time) {
+             if (currentBuzzObject) {
+               currentBuzzObject.setTime(time);
+             }
+           };
+
           return SongPlayer;
      }
 
@@ -131,5 +153,5 @@
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
